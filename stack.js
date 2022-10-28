8c7ab491d16975d93bfb5ac21d25e1f72ad00863
ankurtgs <89077985+AnkurChunekar@users.noreply.github.com>
2022-10-28 14:20:59 +0530

    feat: stack constructor and push method

--- stack.js
+++ stack.js
@@ -10,3 +10,27 @@ LIFO - last in first out.
 Stack is like can of tennis balls
 Commonly used Data structure to create a stack: Array, Linked List 
 */
+
+class Stack {
+  // null pointing end will be the bottom of the stack.
+  constructor(value) {
+    const newNode = new Node(value);
+    this.top = newNode;
+    this.length = 1;
+  }
+
+  //  much like unshift of LL O(1)
+  push(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.top = newNode;
+    } else {
+      newNode.next = this.top;
+      this.top = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+}
