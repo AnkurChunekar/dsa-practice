795abd597cb46184d6ed62043a4706c48ecfb017
ankurtgs <89077985+AnkurChunekar@users.noreply.github.com>
2022-10-28 14:19:36 +0530

    feat: stack basics

---
+++ stack.js
@@ -0,0 +1,12 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+/*
+LIFO - last in first out.
+Stack is like can of tennis balls
+Commonly used Data structure to create a stack: Array, Linked List 
+*/
