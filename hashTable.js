15cc57c52d12517d773dc4304fbf87f11636b2c5
ankurtgs <89077985+AnkurChunekar@users.noreply.github.com>
2022-10-31 21:23:04 +0530

    feat: add hash method

--- hashTable.js
+++ hashTable.js
@@ -2,4 +2,14 @@ class HashTable {
   constructor(size = 7) {
     this.dataMap = new Array(size);
   }
+
+  // _hash function generates and returns an index from 0 - 7 (or whatever size defined) using the key passed to it.
+  // '_' specfies it can only be used by internal methods.
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
+    }
+    return hash;
+  }
 }
