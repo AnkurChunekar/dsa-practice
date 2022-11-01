4944e51eb02a8b4c07015dca84583bd62090a7c3
ankurtgs <89077985+AnkurChunekar@users.noreply.github.com>
2022-11-01 22:41:59 +0530

    feat: add constructor for graph

---
+++ graph.js
@@ -0,0 +1,17 @@
+/* 
+- Vertex is simply a node.
+- The connection between two vertices is called a Edge.
+- An arrow represents the direction is one way.
+- A line represents bi-directional way.
+
+- Two ways to store a graph
+- 1. Adjecency Matrix
+- 2. Adjecency List
+
+*/
+
+class Graph {
+  constructor() {
+    this.adjecencyList = {};
+  }
+}
