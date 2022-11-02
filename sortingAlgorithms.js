4ab320537ef0419d94b218abff79940475d17f90
ankurtgs <89077985+AnkurChunekar@users.noreply.github.com>
2022-11-02 20:19:42 +0530

    bubble sort algorithm

--- sortingAlgorithms.js
+++ sortingAlgorithms.js
@@ -9,3 +9,20 @@ const factorial = (n) => (n === 1 ? 1 : n * sumRange(n - 1));
 const sumRange = (n) => (n === 1 ? 1 : n + sumRange(n - 1));
 const power = (base, exponent) =>
   exponent === 0 ? 1 : base * power(base, exponent - 1);
+
+// Bubble Sort
+
+const bubbleSort = (arr) => {
+  const lastIndex = arr.length - 1;
+  for (let i = 0; i < lastIndex; i++) {
+    for (let j = 0; j < lastIndex - i; j++) {
+      const incJ = j + 1;
+      if (arr[j] > arr[incJ]) {
+        const temp = arr[j];
+        arr[j] = arr[incJ];
+        arr[incJ] = temp;
+      }
+    }
+  }
+  return arr;
+};
