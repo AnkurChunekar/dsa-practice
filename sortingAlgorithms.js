d14af191148048296fd4dcaeb4a0788c55f40047
ankurtgs <89077985+AnkurChunekar@users.noreply.github.com>
2022-11-03 22:20:20 +0530

    Selection sort

--- sortingAlgorithms.js
+++ sortingAlgorithms.js
@@ -26,3 +26,22 @@ const bubbleSort = (arr) => {
   }
   return arr;
 };
+
+// Selection Sort
+
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) minIndex = j;
+    }
+
+    if (i !== minIndex) {
+      const temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+  return arr;
+};
