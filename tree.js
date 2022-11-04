4c4ce03d9e4db2f9c58e304a2eb7065e8cc47f3f
ankurtgs <89077985+AnkurChunekar@users.noreply.github.com>
2022-11-04 23:25:56 +0530

    Tree breath first search

--- tree.js
+++ tree.js
@@ -85,12 +85,20 @@ class BST {
     }
     return temp.value;
   }
-}
 
-const myTree = new BST();
-myTree.insert(47);
-myTree.insert(21);
-myTree.insert(76);
-myTree.insert(52);
-myTree.insert(82);
-myTree.insert(20);
+  BFS() {
+    let currentNode = this.root;
+    const queue = [];
+    const results = [];
+    queue.push(currentNode);
+
+    while (queue.length) {
+      currentNode = queue.shift();
+      results.push(currentNode.value);
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.right);
+    }
+
+    return results;
+  }
+}
